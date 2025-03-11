c07412cf64b7b4538ddcda1852a0b3333c126331
emre kaya <emmre.kayya1@gmail.com>
2025-03-11 15:33:40 +0300

    constructor olusturuldu

--- src/calisan/calisan.java
+++ src/calisan/calisan.java
@@ -1,5 +1,33 @@
 package calisan;
 
 public class calisan {
+public int no;
+public String isim;
+public String soyisim;
+public int tecrube;
+public double maas;
+
+public  calisan(){
+
+}
+
+
+
+    public calisan(int no, String isim, String soyisim, int tecrube, double maas) {
+        this.no = no;
+        this.isim = isim;
+        this.soyisim = soyisim;
+        this.tecrube = tecrube;
+        this.maas = maas;
+    }
+
+    public void calisanBilgileriGoster(){
+System.out.println("calisan bilgileri");
+System.out.println("no"+no);
+        System.out.println("isim"+isim);
+        System.out.println("soyisim"+soyisim);
+        System.out.println("tecrube"+tecrube);
+        System.out.println("maas"+maas);
+}
 
 }
